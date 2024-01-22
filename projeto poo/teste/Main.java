7212d6db7e8b4e86ceeaad3e6c066a44a1bd8843
joaosilvapkn <131157451+joaosilvapkn@users.noreply.github.com>
2024-01-22 14:11:45 +0000

    Delete projeto poo/teste directory

--- projeto poo/teste/Main.java
+++
@@ -1,9 +0,0 @@
-
-
-public class Main
-{
-    public static void main(String[] args) throws ClassNotFoundException, java.io.IOException {
-        Menus a = new Menus();
-        a.menu();
-    }
-}
